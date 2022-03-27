fae36e3ad17dc8b97ac759808acca869c9ab9a7b
srikarpabba <pabbasrikar@gmail.com>
2022-03-28 00:49:25 +0530

    added filtering, sorting, search

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Core.Entities;
 using System.Reflection;
 
+
 namespace Infrastructure.Data
 {
     public class StoreContext : DbContext
@@ -18,6 +19,21 @@ namespace Infrastructure.Data
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.SqlServer")
+            {
+                foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType
+                    == typeof(decimal));
+
+                    foreach (var property in properties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name)
+                        .HasConversion<double>();
+                    }
+                }
+            }
         }
     }
 }
