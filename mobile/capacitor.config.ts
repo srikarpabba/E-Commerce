e9082b4dc6db6e400e69dcae877782dd33c5bc73
srikarpabba <pabbasrikar@gmail.com>
2022-04-01 11:38:49 +0530

    updated mobile structure

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'MyFasho',
+  appName: 'mobile',
   webDir: 'www',
   bundledWebRuntime: false
 };
